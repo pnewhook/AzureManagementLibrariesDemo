f6a92292cb1fc4be1c49ecfebb40d79932a88ca7
Peter Newhook <pnewhook@infusion.com>
2014-04-30 22:59:48 -0400

    Initial check-in of module AzureManagementLibrariesDemo

---
+++ AzureManagementLibrariesDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureManagementLibrariesDemo
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ AzureManagementLibrariesDemo/Swapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Configuration;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Management.Compute;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.WindowsAzure.Management.Compute.Models;
+
+namespace AzurePro.Core
+{
+	public class Swapper
+	{
+		ComputeManagementClient _client;
+
+		public Swapper ()
+		{
+			string certBase64String = ConfigurationManager.AppSettings["certificate"];
+			var cert = new X509Certificate2(Convert.FromBase64String(certBase64String));
+			string subscriptionId = ConfigurationManager.AppSettings["subscriptionId"];
+			SubscriptionCloudCredentials credentials = new CertificateCloudCredentials(subscriptionId, cert);
+			_client = CloudContext.Clients.CreateComputeManagementClient(credentials);
+		}
+
+		public async Task SwapCloudService(string siteName)
+		{
+			var detailed = await _client.HostedServices.GetDetailedAsync(siteName);
+			string stagingName = detailed.Deployments.Single(d => d.DeploymentSlot == DeploymentSlot.Staging).Name;
+			_client.Deployments.SwapAsync(siteName, new DeploymentSwapParameters{ SourceDeployment = stagingName });
+		}
+	}
+}
+
